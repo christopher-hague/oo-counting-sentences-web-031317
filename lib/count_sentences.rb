b1a56119b0eb37c49054daf11cebd3b48414b6d7
christopher-hague <christopher.a.hague@gmail.com>
2017-03-15 23:27:41 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    punc_marks = 0
+    self.split("").each_with_index do |char, i|
+      punc_marks += 1 if char == "." unless self[i - 1] == "."
+      punc_marks += 1 if char == "!" unless self[i - 1] == "!"
+      punc_marks += 1 if char == "?" unless self[i - 1] == "?"
+    end
+    punc_marks
   end
 end
